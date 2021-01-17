9f7532fe9c4ec1d5a25e35d8a37bdfc65f8771b3
jaime999 <martinezsanchezjaime@gmail.com>
2021-01-17 17:59:27 +0100

    Pasar de grados Celsius a Fahrenheit y viceversa.

---
+++ Programa1_2.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Programa1_2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.Scanner; 
+
+public class Programa1_2
+{
+    public static double getFahrenheit(double celsius) {
+        double fahrenheit = (9.0/5.0)*celsius+32;
+        return fahrenheit;
+      
+    }
+    
+    public static double getCelsius(double fahrenheit) {
+        double celsius = (5.0/9.0)*(fahrenheit-32);
+        return celsius;
+      
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Grados Celsius: ");
+        double c = sc.nextDouble();
+        System.out.println("El total en grados Fahrenheit es " + getFahrenheit(c));
+        System.out.println("Grados Fahrenheit: ");
+        double f = sc.nextDouble();
+        System.out.println("El total en grados Celsius es " + getCelsius(f));
+    }
+}
