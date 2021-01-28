bf3b9b2dc5318e182c5edeecd3847e77a9a0801f
jaime999 <martinezsanchezjaime@gmail.com>
2021-01-28 21:01:20 +0100

    Add files via upload

---
+++ PRGParseNum.java
@@ -0,0 +1,173 @@
+
+/**
+ * PRGParseNum: Práctica 2 de PRG, curso 2015-16.
+ * 
+ * @author (professors IIP - PRG) 
+ * @version (curs 2015-16)
+ */
+public class PRGParseNum {
+    
+    /************************** CODIGO INICIAL  **************************/
+    
+    public final static char PUNT = '.';  // Separador parte fraccionaria.
+    public final static char COMA = ',';  // Separador parte fraccionaria.
+    public final static char EXP1 = 'e';  // Separador parte exponencial.
+    public final static char EXP2 = 'E';  // Separador parte exponencial.
+    public final static int  BASE = 10;   // Base de numeración por defecto.
+    
+    /**
+     * Devuelve el entero asociado al carácter dígito en c.
+     * @param c: char.
+     * @return int: valor del dígito.
+     * PRECONDICIÓN: '9' >= c >= '0'
+     */
+    public static int parseDig(char c) {
+        return c - '0';
+        // alternativamente, se puede utilizar:
+        // return Character.getNumericValue(c);
+    }
+    
+    
+    /************************ EJEMPLOS INICIALES *************************/    
+       
+    /** Devuelve la suma de los dígitos contenidos en una String.
+     * @param s String s: Cadena de caracteres sobre los que se opera.
+     * @return int. */
+    public static int sumaDigitos(String s) {
+        // Caso base: String vacía
+        if (s.length() == 0) { return 0; }
+        else {
+        // Caso general: String no vacía. Tratar substring posterior.
+            char c = s.charAt(0);
+            if (c >= '0' && c <= '9') {
+                return (c - '0') + sumaDigitos(s.substring(1));
+            } 
+            else { return sumaDigitos(s.substring(1)); }
+        }
+    }
+        
+    /** Devuelve la suma de los dígitos contenidos en una String.
+     * Versión alternativa utilizando un parámetro posicional.
+     * @param s String s: Cadena de caracteres sobre los que se opera.
+     * @param pos: posición sobre la que se está trabajando en s.
+     * @return int. */
+    public static int sumaDigitos(String s, int pos) {
+        // Caso base: String vacía
+        if (pos >= s.length()) { return 0; }
+        else {
+        // Caso general: String no vacía. Tratar substring posterior.
+            char c = s.charAt(pos);
+            if (c >= '0' && c <= '9') {
+                return (c - '0') + sumaDigitos(s, pos + 1);
+            } 
+            else { return sumaDigitos(s, pos + 1); }
+        }
+    }    
+    
+    /** Devuelve la suma de los dígitos contenidos en una String.
+     * @param s String s: Cadena de caracteres sobre los que se opera.
+     * @return int. */
+    public static int sumaDigitos2(String s) {
+        // Caso base: String vacía
+        if (s.length() == 0) { return 0; }
+        else {
+        // Caso general: String no vacía. Tratar substring anterior.
+            int len = s.length();
+            char c = s.charAt(len - 1);
+            if (c >= '0' && c <= '9') {
+                return (c - '0') + sumaDigitos(s.substring(0, len - 1));
+            } 
+            else { return sumaDigitos(s.substring(0, len - 1)); }
+        }
+    }    
+    
+    /******************  PROBLEMA A: parseInt(String s) ******************/
+        
+    /** 
+     * PRECONDICIÓN: s contiene un entero, >= 0, bien formado. 
+     */
+    public static int parseUnsignedInt(String s) {
+        if (s.length() == 0) { return 0; }
+        else {
+        // Caso general: String no vacía. Tratar substring anterior.
+            int len = s.length();
+            char c = s.charAt(len - 1);
+            if (c >= '0' && c <= '9') {
+                return (c - '0') + parseUnsignedInt(s.substring(0, len - 1))*10;
+            } 
+            else { return parseUnsignedInt(s.substring(0, len - 1)); }
+        }
+    }  
+    
+    /** 
+     * PRECONDICIÓN: s contiene un entero, tal vez con signo, bien formado. 
+     */    
+    public static int parseInt(String s) {
+        if (s.length() == 0) { return 0; }
+        else {
+           //Caso general: String no vacía. Tratar substring anterior.
+            int len = s.length();
+            char c = s.charAt(len - 1);
+            if(s.charAt(0)=='-') {return parseUnsignedInt(s.substring(0, len ))*-1;}
+            else {return parseUnsignedInt(s.substring(0, len ));}
+        }
+    }    
+    
+    /****************  PROBLEMA B: parseDouble(String s) *****************/    
+    
+    /** 
+     * PRECONDICIÓN: s contiene un número en coma flotante bien formado. */
+    public static int posFracSep(String s) {
+        if (s.length() == 0) { return -1; }
+        else {
+        // Caso general: String no vacía. Tratar substring anterior.
+            int len = s.length();
+            char c = s.charAt(len - 1);
+         if (c == PUNT || c== COMA ) {
+                return  len;
+          } 
+          else { return posFracSep(s.substring(0, len - 1)); }
+        }
+    }
+    
+    /** 
+     * PRECONDICIÓN: s, contiene los dígitos de la parte fraccionaria
+     * de un valor double bien formado. 
+     */
+    public static double parseFrac(String s) {
+         if (s.length() == 0) { return 0; }
+        else {
+           //Caso general: String no vacía. Tratar substring anterior.
+           int len = s.length();
+           return parseUnsignedInt(s)*Math.pow(10,-len);
+           
+        }
+    }
+
+    
+    /** 
+     * PRECONDICIÓN: s contiene un double, bien formado, tal vez con signo, 
+     * que puede tener (o no) parte fraccionaria. No contiene parte exponencial.
+     */  
+    public static double parseDouble(String s)   {
+        int entera=0;
+        double decimal=0;
+         if ( posFracSep(s) > 0){
+          entera = parseInt(s.substring(0,posFracSep(s))); }
+            
+             
+         if(posFracSep(s) !=-1){
+             decimal = parseFrac(s.substring(posFracSep(s),s.length())); }
+             
+         if (entera > 0){return entera + decimal;}
+         else {return entera -decimal;}
+    }
+    
+    
+    /*******************  AMPLIACIONES RECOMENDADAS  ********************/
+    
+    
+    
+    
+    
+}
